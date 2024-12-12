aaac46fe0bc15ba3f82458774e3d115126392058
rzayoruk <rizayoruk180@gmail.com>
2024-12-12 05:48:30 +0300

    Add green screen effect and visual examples to webcam stream.

---
+++ 19-WebCam/scripts.js
@@ -0,0 +1,97 @@
+const video = document.querySelector(".player")
+const canvas = document.querySelector(".photo")
+const ctx = canvas.getContext("2d")
+const strip = document.querySelector(".strip")
+const snap = document.querySelector(".snap")
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream
+      video.play()
+    })
+    .catch((err) => {
+      console.error("err!")
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+  canvas.width = width
+  canvas.height = height
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+    let pixels = ctx.getImageData(0, 0, width, height)
+
+    //pixels = redEffect(pixels)
+    //pixels = rgbSplit(pixels)
+    pixels = greenScreen(pixels)
+
+    ctx.globalAlpha = 0.1
+    ctx.putImageData(pixels, 0, 0)
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0
+  snap.play()
+  const data = canvas.toDataURL("image/jpeg") // returns b64 encoded data of the image
+  const link = document.createElement("a")
+  link.href = data
+  link.setAttribute("download", "somebody")
+  link.textContent = "Download image"
+  link.innerHTML = `<img src="${data}" alt="somebody"/>`
+  strip.insertBefore(link, strip.firstChild)
+  console.log(link)
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100 //R
+    pixels.data[i + 1] = pixels.data[i + 1] - 50 //G
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5 //B
+  }
+  return pixels
+}
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0] //R
+    pixels.data[i + 100] = pixels.data[i + 1] //G
+    pixels.data[i - 550] = pixels.data[i + 2] //B
+  }
+  return pixels
+}
+
+function greenScreen(pixels) {
+  const levels = {}
+  document.querySelectorAll(".rgb input").forEach((input) => {
+    levels[input.name] = input.value
+  })
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0]
+    green = pixels.data[i + 1]
+    blue = pixels.data[i + 2]
+    alpha = pixels.data[i + 3]
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0 //wow
+    }
+  }
+
+  return pixels
+}
+
+video.addEventListener("canplay", paintToCanvas) // nice to learn canplay
+getVideo()
